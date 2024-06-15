cb243fb9771414c5fbf6f2e800b9be7518624fc0
Vasiliy Anohin <bandavasa@gmail.com>
2024-06-15 09:34:45 +0300

    project added 2

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,79 @@
-package 
+package ru.netology.stats;
 
 public class StatsService {
+
+// 1.Сумму всех продаж.
+
+    public long sum(long[] sales) {
+        long result = 0;
+        for (long sale : sales) {
+            result += sale;
+        }
+        return result;
+    }
+
+
+// 2.Среднюю сумму продаж в месяц.
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+
+// 3.Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму.
+
+public int maxSales(long[] sales) {
+    int maxMonth = 0;
+    long maxSale = 0;
+
+    for (int i = 0; i < sales.length; i++) {
+        if (sales [i] >= maxSale) {
+            maxSale = sales[i];
+            maxMonth = i;
+        }
+    }
+    return maxMonth + 1;
+
+}
+
+// 4.Номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму.
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+// 5.Количество месяцев, в которых продажи были ниже среднего.
+    public int belowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+            
+        }
+        return counter;
+    }
+
+// 6.Количество месяцев, в которых продажи были выше среднего.
+    public int aboveAverage(long[] sales) {
+    int counter = 0;
+    for (long sale : sales) {
+        if (sale > average(sales)) {
+            counter++;
+        }
+
+    }
+    return counter;
+}
+
+
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,91 @@
-package PACKAGE_NAME;public class StatsServiceTest {
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatsServiceTest {
+
+// 1.Сумму всех продаж.
+
+    @Test
+    public void testSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        long actual = service.sum(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+// 2.Среднюю сумму продаж в месяц.
+
+    @Test
+    public void testAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        long actual = service.average(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+// 3.Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму.
+
+    @Test
+    public void testMax() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8;
+        long actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+// 4.Номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму.
+
+    @Test
+    public void testMin() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 9;
+        long actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+// 5.Количество месяцев, в которых продажи были ниже среднего.
+
+    @Test
+    public void testBelowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.belowAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+// 6.Количество месяцев, в которых продажи были выше среднего.
+
+    @Test
+    public void testAboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actual = service.belowAverage(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
